876aa7e17cda7b896b244d7f9dce9673d330677a
JacksionGT <superboy537@sina.com>
2017-10-07 20:01:13 +0800

    添加一个拖拽Ajax上传图片的ASP.NET MVC的demo

---
+++ ASP.NET/DropNAjaxUpload/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using DragUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static ImageModel imageModel = new ImageModel();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPVforFileBrowsing()
+        {
+            return View();
+        }
+
+        public ActionResult GetPVDragDrop()
+        {
+            return View();
+        }
+
+        public ActionResult GetPVUsingWebCam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UploadImage()
+        {
+            foreach (string upload in Request.Files)
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "ImagesUploaded/";
+                string filename = Path.GetFileName(Request.Files[upload].FileName);
+                Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                imageModel.ImagePath = filename;
+                imageModel.ImageName = Request.Form["ImageName"];
+            }
+        }
+
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        public ActionResult ViewSavedImage()
+        {
+            return PartialView("ViewSavedImage", imageModel);
+        }
+
+        // Since Camera Capure Partial View is using
+        // which put the image in InputStream instead of Files
+        public ActionResult CameraCapture(string imageName)
+        {
+
+            var stream = Request.InputStream;
+            string imageFileInStringFormat;
+
+            using (var reader = new StreamReader(stream))
+                imageFileInStringFormat = reader.ReadToEnd();
+
+            string AppPath = AppDomain.CurrentDomain.BaseDirectory + "ImagesUploaded/";
+            var path = Path.Combine(AppPath, imageName + ".jpg");
+            System.IO.File.WriteAllBytes(path, stringToByteArrayConvertor(imageFileInStringFormat));
+
+            imageModel.ImagePath = imageName + ".jpg";
+            imageModel.ImageName = imageName;
+
+            return null;
+        }
+
+        private byte[] stringToByteArrayConvertor(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+            byte[] bytes = new byte[numBytes];
+
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ ASP.NET/DropNAjaxUpload/scripts/js/web-cam.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+
+    $("#btnViewImage").hide();
+    $("#validateImageInput").hide();
+    $("#validateCameraInput").hide();
+
+    // assigning properties and adding callback fuctions
+    $("#cameraArea").webcam({        
+        width: 250,
+        height: 160,
+        mode: "save",
+        swffile: "/Scripts/ExternalJs/jscam.swf",
+        onTick: function () { },
+        onSave: function () {
+        },
+        onCapture: function ()
+        {
+            var imageName = $("#txtImageName").val();
+            var url = getContextPath() + "CameraCapture/" + imageName;          
+            webcam.save(url);
+            $("#btnViewImage").show();
+        },
+        debug: function (type, message)
+        {
+            if (message === "Camera started") { window.webcam.started = true; }            
+        },
+        onLoad: function () { }
+    });
+
+    // Validating and calling capture method of webcam
+    $("#btnCaptureSave").click(function () {      
+        var imageName = $("#txtImageName").val();
+        if (imageName === "")
+        {
+            $("#validateImageInput").show();
+        }
+        else
+        {
+            $("#validateImageInput").hide();
+            if (!webcam.started) {            
+                $("#validateCameraInput").show();
+            }
+            else
+            {
+                $("#validateCameraInput").hide();
+                $("#btnViewImage").hide();
+                webcam.capture();
+            }
+        }
+    });
+
+    
+    function getContextPath()
+    {
+        var ctx = window.location.pathname,
+            path = '/' !== ctx ? ctx.substring(0, ctx.indexOf('/', 1) + 1) : ctx;
+        return path + (/\/$/.test(path) ? '' : '/');
+    }
+    
+    $("#txtImageName").keydown(function () {
+        $("#validateImageInput").hide();
+    });
+
+    $("#btnViewImage").click(function () {
+        $.ajax({
+            type: "Get",
+            url: '/Home/ViewSavedImage',
+            dataType: "html",
+            contentType: false,
+            processData: false,
+            success: function (result)
+            {
+                $("#pvContainerDiv").empty().append(result);
+            }
+        });
+    });
+});
